685a17e41cd9540aaa2bd769a2c94b0147bb2618
Ramon Carvalho <114238263+rcarvalhox@users.noreply.github.com>
2022-11-06 15:02:24 -0300

    Projeto-Grupo-mod2
    Equipe: WASHINGTON LUIZ,  THAYSSA GARCIA , THAYMARA MOURA, VICTOR HUGO, JOSÉ LUCAS, RAMON CARVALHO.

---
+++ images/script.js
@@ -0,0 +1,27 @@
+const users = [
+  {
+    nome: "Abraão Farias",
+    idade: 22,
+    animal: "gato"
+  },
+  {
+    nome: "Michelina Souza",
+    idade: 12,
+    animal: "Hamster"
+  },
+  {
+    nome: "Venâncio Almeida",
+    idade: 45,
+    animal: ""
+  },
+  {
+    nome: "Maria Silva",
+    idade: 15,
+    animal: "Coelho"
+  },
+  {
+    nome: "José Eustáquio",
+    idade: 32,
+    animal: "Gato"
+  },
+]
